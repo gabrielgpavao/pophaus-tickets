655530ce492828b892ab9080bada2825e70760f4
gabriel <gabpavao@outlook.com>
2023-12-13 17:27:15 -0300

    feat: create abstract methods in the TemplateRepository abstract class

--- back/src/resources/templates/repositories/knex/templates-knex.repository.ts
+++ back/src/resources/templates/repositories/knex/templates-knex.repository.ts
@@ -1,13 +1,31 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
 import { Knex } from 'knex'
 import { TemplatesRepository } from '../templates.repository'
 import { database } from '../../../../database'
 import { Template } from '../../entities/templates.entity'
+import { Hour } from '../../entities/hours.entity'
 
 class TemplatesKnexRepository implements TemplatesRepository {
     private queryBuilder: Knex.QueryBuilder
 
     constructor(tableName: string) {
         this.queryBuilder = database<Template>(tableName)
+    }
+
+    async listHoursByDate(date: string): Promise<Hour> {
+        throw new Error('Method not implemented.')
+    }
+
+    async listDatesByUserEmail(email: string): Promise<{ date: string }[]> {
+        throw new Error('Method not implemented.')
+    }
+
+    async counterClients(
+        date: string,
+    ): Promise<
+        Record<'players' | 'not_players' | 'pcd' | 'total_clients', string>
+    > {
+        throw new Error('Method not implemented.')
     }
 }
 
--- back/src/resources/templates/repositories/templates.repository.ts
+++ back/src/resources/templates/repositories/templates.repository.ts
@@ -1,1 +1,13 @@
-export class TemplatesRepository {}
+import { Hour } from '../entities/hours.entity'
+
+export abstract class TemplatesRepository {
+    abstract listHoursByDate(date: string): Promise<Hour>
+
+    abstract listDatesByUserEmail(email: string): Promise<{ date: string }[]>
+
+    abstract counterClients(
+        date: string,
+    ): Promise<
+        Record<'players' | 'not_players' | 'pcd' | 'total_clients', string>
+    >
+}
